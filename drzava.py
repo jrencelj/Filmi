8997386de045054c21bc1d7351acac8688b9cb15
Jernej <jrencelj@gmail.com>
2023-01-05 10:17:10 +0100

    Prvi commit.

---
+++ drzava.py
@@ -0,0 +1,26 @@
+import sqlite3 as dbapi
+class Drzava:
+
+    def __init__(self, drzava_ime):
+        self._drzava_ime = drzava_ime
+
+    def shrani_drzava(self):
+        '''Shrani državo v bazo.'''
+        conn = dbapi.connect('filmi.db') 
+        with conn:
+            conn.execute("""
+            INSERT OR IGNORE INTO drzava (drzava_ime)
+            VALUES (?)
+            """, [self._drzava_ime])
+
+    @staticmethod
+    def pridobi_drzava_id(drzava_ime):
+        '''Pridobi id države iz baze.'''
+        conn = dbapi.connect('filmi.db')
+        with conn:
+            cursor = conn.execute("""
+                SELECT id FROM drzava
+                WHERE drzava_ime=?
+            """, [drzava_ime])
+            podatki = cursor.fetchone()
+            return Drzava(podatki[0])
