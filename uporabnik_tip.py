7b661f917f20ce6280d0fff86517e324307db0a0
Tom Rupnik <tomrupnik1@gmail.com>
2023-01-05 18:33:59 +0100

    Dodane get in set metode

--- uporabnik_tip.py
+++ uporabnik_tip.py
@@ -1,4 +1,6 @@
 import sqlite3 as dbapi
+
+
 class Uporabnik_Tip:
     def __init__(self, sifra, naziv, opis):
         self._sifra = sifra
@@ -7,11 +9,33 @@ class Uporabnik_Tip:
 
     def shrani_uporabnik_tip(self):
         '''Shrani uporabnik tip v bazo.'''
-        conn = dbapi.connect('filmi.db') 
+        conn = dbapi.connect('filmi.db')
         with conn:
             conn.execute("""
             INSERT INTO uporabnik_tip (sifra, naziv, opis)
             VALUES (?, ?, ?)
             """, [self._sifra, self._naziv, self._opis])
-        
-        
+
+    @property
+    def sifra(self):
+        return self._sifra
+
+    @property
+    def naziv(self):
+        return self._naziv
+
+    @property
+    def opis(self):
+        return self._opis
+
+    @property.setter
+    def sifra(self, vrednost):
+        self._sifra = vrednost
+
+    @property.setter
+    def naziv(self, vrednost):
+        self._naziv = vrednost
+
+    @property.setter
+    def opis(self, vrednost):
+        self._opis = vrednost
