15ac7663516b56914c7da896bd2504a487588fdb
Jernej <jrencelj@gmail.com>
2023-01-05 13:10:04 +0100

    Sprememba starševskega razreda.

--- serija.py
+++ serija.py
@@ -1,6 +1,6 @@
-from film import Film
+from vsebina import Vsebina
 
-class Serija(Film):
+class Serija(Vsebina):
     def __init__(self, naslov, dolzina, imdb_id, opis, datum_prvega_predvajanja, leto_izida,
                 certifikat, vsebina_tip):
         super()._naslov = naslov
