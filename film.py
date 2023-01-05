b709d76ae8e8c5c25cc8cc6e08714117217adc18
Tom Rupnik <tomrupnik1@gmail.com>
2023-01-05 17:15:37 +0100

    Dodane get in set metode

--- film.py
+++ film.py
@@ -6,9 +6,10 @@ import sqlite3 as dbapi
 from bralnik import Bralnik
 from vsebina_tip import Vsebina_Tip
 
+
 class Film(Vsebina):
     def __init__(self, naslov, dolzina, url_slika, imdb_id_vsebina, opis, datum_prvega_predvajanja, vsebina_tip, leto_izida,
-                certifikat):
+                 certifikat):
         if not isinstance(certifikat, Certifikat):
             raise Exception(f'{certifikat} ni objekt razreda Certifikat')
         else:
@@ -17,19 +18,37 @@ class Film(Vsebina):
             raise Exception(f'{leto_izida} ni objekt razreda int')
         else:
             self._leto_izida = leto_izida
-        super().__init__(naslov, dolzina, url_slika, imdb_id_vsebina, opis, datum_prvega_predvajanja, vsebina_tip)
+        super().__init__(naslov, dolzina, url_slika, imdb_id_vsebina,
+                         opis, datum_prvega_predvajanja, vsebina_tip)
 
     def shrani_film(self):
         '''Shrani film v bazo.'''
-        conn = dbapi.connect('filmi.db') 
+        conn = dbapi.connect('filmi.db')
         with conn:
             conn.execute("""
             INSERT INTO vsebina (naslov, dolzina, leto_izida, url_slika, imdb_id_vsebina, opis, datum_prvega_predvajanje, certifikat_id,
                             vsebina_tip_id)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
             """, [super().get_naslov(), super().get_dolzina(), self._leto_izida, super().get_url_slika(), super().get_imdb_id_vsebina(),
-                    super().get_opis(), Bralnik.v_tip_datum_tri_crke(self._datum_prvega_predvajanja), self._certifikat.pridobi_certifikat_id(), 
-                    super().get_vsebina_tip().pridobi_vsebina_tip_id()])
+                  super().get_opis(), Bralnik.v_tip_datum_tri_crke(
+                      self._datum_prvega_predvajanja), self._certifikat.pridobi_certifikat_id(),
+                  super().get_vsebina_tip().pridobi_vsebina_tip_id()])
+
+    @property
+    def certifikat(self):
+        return self._certifikat
+
+    @property
+    def leto_izida(self):
+        return self._leto_izida
+
+    @property.setter
+    def certifikat(self, vrednost):
+        self._certifikat = vrednost
+
+    @property.setter
+    def leto_izida(self, vrednost):
+        self._leto_izida = vrednost
 
     @staticmethod
     def pridobi_vse_filme():
@@ -41,7 +60,7 @@ class Film(Vsebina):
             """)
             podatki = list(cursor.fetchall())
             return [
-                Film(podatek[1], podatek[2], podatek[4], podatek[5], podatek[6], podatek[7], 
-                    Vsebina_Tip.pridobi_vsebina_tip_po_id(podatek[11]), podatek[3], Certifikat.pridobi_certifikat_po_id(podatek[10]))
+                Film(podatek[1], podatek[2], podatek[4], podatek[5], podatek[6], podatek[7],
+                     Vsebina_Tip.pridobi_vsebina_tip_po_id(podatek[11]), podatek[3], Certifikat.pridobi_certifikat_po_id(podatek[10]))
                 for podatek in podatki
             ]
