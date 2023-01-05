3ed52b08a010a75be9e283f9e58113f82abe8098
Tom Rupnik <tomrupnik1@gmail.com>
2023-01-05 17:24:58 +0100

    Dodane get in set metode

--- kinoteka.py
+++ kinoteka.py
@@ -3,6 +3,8 @@ from bralnik import Bralnik
 import json
 from bs4 import BeautifulSoup
 import re
+
+
 class Kinoteka:
 
     def __init__(self, ime, url):
@@ -11,7 +13,7 @@ class Kinoteka:
 
     def shrani_kinoteka(self):
         '''Shrani kinoteko v bazo.'''
-        conn = dbapi.connect('filmi.db') 
+        conn = dbapi.connect('filmi.db')
         with conn:
             conn.execute("""
             INSERT INTO kinoteka (ime, url)
@@ -28,15 +30,17 @@ class Kinoteka:
         vsebina_kinoteka = dict()
         vsebina_kinoteka[kinoteka] = list()
         for leto in range(1900, 2023):
-            odziv = Bralnik.pridobi_html(f'https://www.justwatch.com/si/ponudnik/{kinoteka}/filmi?release_year_from={leto}&release_year_until={leto}&rating_imdb=8')
+            odziv = Bralnik.pridobi_html(
+                f'https://www.justwatch.com/si/ponudnik/{kinoteka}/filmi?release_year_from={leto}&release_year_until={leto}&rating_imdb=8')
             soup = BeautifulSoup(odziv, 'html.parser')
-            slike = soup.find_all('picture', class_ = 'picture-comp title-poster__image')
+            slike = soup.find_all(
+                'picture', class_='picture-comp title-poster__image')
             for slika in slike:
                 regex_naslov = r'<img alt=\"([^\"]*)\"[^>]*>'
                 img = slika.find_all('img')
                 naslov = re.findall(regex_naslov, str(img))
                 vsebina_kinoteka[kinoteka].append(naslov[0])
-        with open(f'data/film/kinoteka/{kinoteka}.json', 'w', encoding = 'utf8') as f:
+        with open(f'data/film/kinoteka/{kinoteka}.json', 'w', encoding='utf8') as f:
             json.dump(vsebina_kinoteka, f)
 
     def pridobi_serije(self):
@@ -49,13 +53,31 @@ class Kinoteka:
         vsebina_kinoteka = dict()
         vsebina_kinoteka[kinoteka] = list()
         for leto in range(1900, 2023):
-            odziv = Bralnik.pridobi_html(f'https://www.justwatch.com/si/ponudnik/{kinoteka}/serije?release_year_from={leto}&release_year_until={leto}&rating_imdb=8.4')
+            odziv = Bralnik.pridobi_html(
+                f'https://www.justwatch.com/si/ponudnik/{kinoteka}/serije?release_year_from={leto}&release_year_until={leto}&rating_imdb=8.4')
             soup = BeautifulSoup(odziv, 'html.parser')
-            slike = soup.find_all('picture', class_ = 'picture-comp title-poster__image')
+            slike = soup.find_all(
+                'picture', class_='picture-comp title-poster__image')
             for slika in slike:
                 regex_naslov = r'<img alt=\"([^\"]*)\"[^>]*>'
                 img = slika.find_all('img')
                 naslov = re.findall(regex_naslov, str(img))
                 vsebina_kinoteka[kinoteka].append(naslov[0])
-        with open(f'data/serija/kinoteka/{kinoteka}.json', 'w', encoding = 'utf8') as f:
+        with open(f'data/serija/kinoteka/{kinoteka}.json', 'w', encoding='utf8') as f:
             json.dump(vsebina_kinoteka, f)
+
+    @property
+    def ime(self):
+        return self._ime
+
+    @property
+    def url(self):
+        return self._url
+
+    @property.setter
+    def ime(self, vrednost):
+        self._ime = vrednost
+
+    @property.setter
+    def url(self, vrednost):
+        self._url = vrednost
