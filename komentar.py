b84abaf75c0081610f5e19fccde89e6ffd5a9dab
Tom Rupnik <tomrupnik1@gmail.com>
2023-01-05 17:36:27 +0100

    Dodane get in set metode

--- komentar.py
+++ komentar.py
@@ -1,5 +1,7 @@
 from film import Film
 from uporabnik import Uporabnik
+
+
 class Komentar:
     def __init__(self, uporabnik, naslov_komentar, besedilo_komentar, ocena, film, ura_datum):
         self._uporabnik = uporabnik
@@ -20,3 +22,51 @@ class Komentar:
         '''Vstavi komentar v bazo.'''
         # TODO
         pass
+
+    @property
+    def uporabnik(self):
+        return self._uporabnik
+
+    @property
+    def naslov_komentar(self):
+        return self._naslov_komentar
+
+    @property
+    def besedilo_komentar(self):
+        return self._besedilo_komentar
+
+    @property
+    def ocena(self):
+        return self._ocena
+
+    @property
+    def film(self):
+        return self._film
+
+    @property
+    def ura_datum(self):
+        return self._ura_datum
+
+    @property.setter
+    def uporabnik(self, vrednost):
+        self._uporabnik = vrednost
+
+    @property.setter
+    def naslov_komentar(self, vrednost):
+        self._naslov_komentar = vrednost
+
+    @property.setter
+    def besedilo_komentar(self, vrednost):
+        self._besedilo_komentar = vrednost
+
+    @property.setter
+    def ocena(self, vrednost):
+        self._ocena = vrednost
+
+    @property.setter
+    def film(self, vrednost):
+        self._film = vrednost
+
+    @property.setter
+    def ura_datum(self, vrednost):
+        self._ura_datum = vrednost
