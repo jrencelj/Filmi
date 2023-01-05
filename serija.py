679233daa715b6627832f96cff3d2e162ced209d
Jernej <jrencelj@gmail.com>
2023-01-05 10:21:12 +0100

    Prvi commit.

---
+++ serija.py
@@ -0,0 +1,15 @@
+from film import Film
+
+class Serija(Film):
+    def __init__(self, naslov, dolzina, imdb_id, opis, datum_prvega_predvajanja, leto_izida,
+                certifikat, vsebina_tip):
+        super()._naslov = naslov
+        super()._dolzina = dolzina
+        # TODO
+        
+    def vstavi_serija(self):
+        '''Vstavi serijo v bazo.'''
+        # TODO
+        pass
+
+    
