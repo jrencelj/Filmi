84521ea8228633b77c3d23ccabf7ea48aca3d1e1
Tom Rupnik <tomrupnik1@gmail.com>
2023-01-06 11:26:08 +0100

    Dodal SQL pridobi id

--- vloga_tip.py
+++ vloga_tip.py
@@ -1,22 +1,32 @@
 import sqlite3 as dbapi
 
+
 class Vloga_Tip:
-    
+
     def __init__(self, sifra, naziv):
         self._sifra = sifra
         self._naziv = naziv
 
     def shrani_vloga_tip(self):
         '''Shrani tip vloge v bazo.'''
-        conn = dbapi.connect('filmi.db') 
+        conn = dbapi.connect('filmi.db')
         with conn:
             conn.execute("""
             INSERT INTO vloga_tip (sifra, naziv)
             VALUES (?, ?)
             """, [self._sifra, self._naziv])
 
-    # TODO pridobi_vloga_tip_id(self), WHERE po 'naziv'
-    
+    def pridobi_vloga_tip_id(self):
+        '''Pridobi id tipa vloge iz baze'''
+        conn = dbapi.connect('filmi.db')
+        with conn:
+            cursor = conn.execute("""
+                SELECT id FROM vloga_tip
+                WHERE naziv=?
+            """, [self._naziv])
+            id = cursor.fetchone()[0]
+            return id
+
     @property
     def sifra(self):
         return self._sifra
@@ -32,6 +42,3 @@ class Vloga_Tip:
     @naziv.setter
     def naziv(self, vrednost):
         self._naziv = vrednost
-
-    
-    
