37800b2f8b2cf741ffdce18118406a4683969832
Jernej <jrencelj@gmail.com>
2023-01-09 13:45:17 +0100

    Dopolnil z novimi funkcijami za delo z bazo.

--- serija.py
+++ serija.py
@@ -1,34 +1,25 @@
 from vsebina import Vsebina
 import sqlite3 as dbapi
 from bralnik import Bralnik
+from certifikat import Certifikat
+from vsebina_tip import Vsebina_Tip
 
 class Serija(Vsebina):
-    def __init__(self, naslov, dolzina, imdb_id, opis, datum_prvega_predvajanja, leto_izida,
-                 certifikat, vsebina_tip):
-        super()._naslov = naslov
-        super()._dolzina = dolzina
-        # TODO
 
-    def shrani_serija(self):
-        '''Shrani serijo v bazo.'''
-        conn = dbapi.connect('filmi.db')
-        with conn:
-            conn.execute("""
-            INSERT INTO vsebina (naslov, dolzina, leto_izida, url_slika, imdb_id_vsebina, opis, datum_prvega_predvajanje, certifikat_id,
-                            vsebina_tip_id)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-            """, [super()._naslov, super().get_dolzina(), self._leto_izida, super().get_url_slika(), super().get_imdb_id_vsebina(),
-                  super().get_opis(), Bralnik.v_tip_datum_tri_crke(
-                      self._datum_prvega_predvajanja), self._certifikat.pridobi_certifikat_id(),
-                  super().get_vsebina_tip().pridobi_vsebina_tip_id()])
+    def __init__(self, naslov, dolzina, url_slika, imdb_id_vsebina, opis, datum_prvega_predvajanja, vsebina_tip, leto_izida,
+                 certifikat):
+        if not isinstance(certifikat, Certifikat):
+            raise Exception(f'{certifikat} ni objekt razreda Certifikat!')
+        else:
+            self._certifikat = certifikat
+        if not isinstance(leto_izida, int):
+            raise Exception(f'{leto_izida} ni objekt razreda int!')
+        else:
+            self._leto_izida = leto_izida
+        super().__init__(naslov, dolzina, url_slika, imdb_id_vsebina,
+                         opis, datum_prvega_predvajanja, vsebina_tip)
 
-    @property
-    def imdb_id(self):
-        return self._imdb_id
-
-    @imdb_id.setter
-    def imdb_id(self, vrednost):
-        self._imdb_id = vrednost
+        #TODO
 
     @property
     def leto_izida(self):
@@ -45,3 +36,31 @@ class Serija(Vsebina):
     @certifikat.setter
     def certifikat(self, vrednost):
         self._certifikat = vrednost
+
+    def shrani_serija(self):
+        '''Shrani serijo v bazo.'''
+        conn = dbapi.connect('filmi.db')
+        with conn:
+            conn.execute("""
+            INSERT INTO vsebina (naslov, dolzina, leto_izida, url_slika, imdb_id_vsebina, opis, datum_prvega_predvajanje, certifikat_id,
+                            vsebina_tip_id)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """, [self.naslov, self.dolzina, self.leto_izida, self.url_slika, self.imdb_id_vsebina,
+                  self.opis, Bralnik.v_tip_datum_tri_crke(
+                      self.datum_prvega_predvajanja), self.certifikat.pridobi_certifikat_id(),
+                  self.vsebina_tip.pridobi_vsebina_tip_id()])
+
+    @staticmethod
+    def pridobi_vse_serije():
+        '''Pridobi vse filme iz baze in jih vrne v seznama.'''
+        conn = dbapi.connect('filmi.db')
+        with conn:
+            cursor = conn.execute("""
+            SELECT * FROM vsebina WHERE vsebina_tip_id = 2
+            """)
+            podatki = list(cursor.fetchall())
+            return [
+                Serija(podatek[1], podatek[2], podatek[4], podatek[5], podatek[6], podatek[7],
+                     Vsebina_Tip.pridobi_vsebina_tip_po_id(podatek[11]), podatek[3], Certifikat.pridobi_certifikat_po_id(podatek[10]))
+                for podatek in podatki
+            ]
